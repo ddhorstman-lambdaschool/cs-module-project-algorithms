4bde3ca6edede5fff0b6c50148b287f73d69b465
David Horstman <davidzilla27@gmail.com>
2020-07-22 13:23:17 -0400

    Eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,37 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
-    # Your code here
+    number_of_ways = 0
+
+    def eat(total, to_eat):
+        nonlocal number_of_ways
+        total -= to_eat
+        if total == 0:
+            number_of_ways += 1
+        elif total < 0:
+            return
+        else:
+            eat(total, 1)
+            eat(total, 2)
+            eat(total, 3)
+
+    if n <= 1:
+        return 1
+    else:
+        eat(n, 1)
+        eat(n, 2)
+        eat(n, 3)
+        return number_of_ways
 
     pass
 
+
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 2
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
